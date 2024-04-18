f26e6bcc3c97247aa4d15b407472c1fe1cde888a
ArihaanB <139609731+ArihaanB@users.noreply.github.com>
2024-04-18 20:39:54 +1000

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+//import { initializeApp } from "firebase/app";
+
+///const firebaseConfig = {
+  //apiKey: "AIzaSyBQeteMvMPQYc3Rr9xBgdgTA0VxM5OXElA",
+  //authDomain: "p94byju-s.firebaseapp.com",
+  //projectId: "p94byju-s",
+  //storageBucket: "p94byju-s.appspot.com",
+  //messagingSenderId: "322718852708",
+  //appId: "1:322718852708:web:b2e7e08593bb98d104e171"
+//};
+
+
+//const app = initializeApp(firebaseConfig);
